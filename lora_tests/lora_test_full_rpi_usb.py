0058a91a5ca3ca99d10a26fd13b529f213ffd6a6
karoldus <karol.duszczyk.stud@pw.edu.pl>
2021-12-09 17:56:09 +0100

    functions def

--- lora_tests/lora_test_full_rpi_usb.py
+++ lora_tests/lora_test_full_rpi_usb.py
@@ -3,50 +3,118 @@ To Do:
 - timeout
 - write_timeout
 '''
-
-
 import serial
 import io
 import time
 
+ANSWER_TIMEOUT = 1.2
+JOINING_TIMEOUT = 20
+MESSAGE_TIMEOUT = 10
+
 def test_device(ser):
+    ser.reset_input_buffer()
+    ser.write(b'AT\n')
+    ser.flush() 
+    time.sleep(ANSWER_TIMEOUT)
+    while ser.inWaiting() > 0:  
+        ans = ser.readline().decode('ascii')
+        # print(ans)
+        if "+AT: OK" in ans:
+            ser.reset_input_buffer()
+            return "Device is working"
+    return "Device isn't working"
+
+
+def get_device_id(ser): # TO DO
+    ser.reset_input_buffer()
     ser.write(b'AT+ID\n')
     ser.flush() 
+    time.sleep(ANSWER_TIMEOUT)
+    while ser.inWaiting() > 0:  
+        ans = ser.readline().decode('ascii')
+        print(ans)
 
 
-with serial.Serial('/dev/ttyUSB0', 9600, timeout=None) as ser:
-    # print(ser.name)
-    # print(ser)
+def join_network(ser):                      # co w przypadku, gdy już jest dołączony?
+    print("JOINING TO NETWORK...")
+    ser.reset_input_buffer()
+    ser.write(b'AT+JOIN\n')
+    ser.flush()
 
-    while True:
+    start = time.time()
+    
+    while (time.time() - start < JOINING_TIMEOUT):
+        while ser.inWaiting() > 0:
+            ans = ser.readline().decode('ascii')
+            if "Join failed" in ans:
+                print("JOIN FAILED")
+                return 0
+            elif "Start" in ans:
+                print("STARTING...")
+            elif "joined" in ans:
+                print("Joined network")
+            elif "NetID" in ans:
+                print(ans)
+                return 1
+    
+    print("Timeout joining network.")
+    return -1
+
+
+def send_mess_string(ser, mess):
+    print("SENDING MESSAGE {mess}...")
+    mess = str(mess)
+    command = 'AT+MSG='+mess+'\n'
+    ser.reset_input_buffer()
+    ser.write(command.encode())
+    ser.flush()
+
+    start = time.time()
+    
+    while (time.time() - start < MESSAGE_TIMEOUT):
+        while ser.inWaiting() > 0:
+            ans = ser.readline().decode('ascii')
+            if "Please join network first" in ans:
+                print("Please join network first")
+                return 0
+            elif "Start" in ans:
+                print("STARTING...")
+            elif "Done" in ans:
+                print("SENT")
+                return 1
+            elif "RX" in ans:
+                print("DOWNLINK PAYLOAD: ", ans) # TO DO - analyze payload
+                return 1
+    
+    print("Timeout sending message.")
+    return -1
+
+
+
+
+
+if __name__ == '__main__':
+
+    with serial.Serial('/dev/ttyUSB0', 9600, timeout=None) as ser:
         try:
-            ser.write(b'AT+ID')
-            ser.flush()   
+            print("CONNECTED TO: ", ser.name)
+            print(ser)
+            print(test_device(ser))
+            get_device_id(ser)
 
-            # v1 - nwm czy działa:
-            # time.sleep(0.2) 
-            # print(ser.is_open)     
-            # stm = ser.readline().decode('ascii')
-            # print("cp4")
-            # print(stm)
+            while join_network(ser) != 1:
+                print(test_device(ser))
+                time.sleep(3)
 
-            #v2 - działa, ale ma problem z oczekiwaniem
-            time.sleep(1.5)
-            if ser.inWaiting() > 0:
-                received_data = ser.read()              #read serial port
-                time.sleep(0.03)
-                data_left = ser.inWaiting()             #check for remaining byte
-                received_data += ser.read(data_left)
-                print (received_data)
-            else:
-                print("error oooo")
+            num = 1
 
-            
-            time.sleep(0.1)
+            while True:
 
-
-
+                send_mess_string(ser, "hejka_"+str(num))
+                num = num + 1
+                print("idle")
+                time.sleep(60)
+                
         except:
-                print("board is disconnected")
+                print("Disconnected")
                 ser.close()
-                break
