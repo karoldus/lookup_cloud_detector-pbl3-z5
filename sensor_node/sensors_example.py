eeb9efa949e14c441f653e412795fbcc349d03d5
karoldus <karol.duszczyk.stud@pw.edu.pl>
2021-12-12 10:37:23 +0100

    example - using sensors

---
+++ sensor_node/sensors_example.py
@@ -0,0 +1,11 @@
+import sensors
+import time
+
+ir_obj = sensors.ir_init()
+temp_obj = sensors.temp_init()
+
+while True:
+    t1 = sensors.ir_read(ir_obj)
+    t2 = sensors.temp_read(temp_obj)
+    print(f"IR: {t1}, DS18B20: {t2}")
+    time.sleep(1)
