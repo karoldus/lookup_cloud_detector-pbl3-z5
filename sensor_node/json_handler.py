22e6ad9b2aaad73a97714eb24834d2010877decd
karoldus <karol.duszczyk.stud@pw.edu.pl>
2021-12-20 18:35:20 +0100

    comments

--- sensor_node/json_handler.py
+++ sensor_node/json_handler.py
@@ -1,6 +1,10 @@
 import json
 
 def configuration_save(key, value):
+    """ 
+    Save key:value pair to configuration.json file
+    Parameters: key, value
+    """
     with open("configuration.json", "r") as read_file:
         data = json.load(read_file)
     with open("configuration.json", "w") as write_file:
@@ -8,6 +12,11 @@ def configuration_save(key, value):
         json.dump(data, write_file)
     
 def configuration_read(key):
+    """ 
+    Read value for key from configuration.json file
+    Parameters: key
+    Return: key's value
+    """
     with open("configuration.json", "r") as read_file:
         data = json.load(read_file)
         return data[key]
--- server/json_handler.py
+++ server/json_handler.py
@@ -1,6 +1,10 @@
 import json
 
 def configuration_save(key, value):
+    """ 
+    Save key:value pair to configuration.json file
+    Parameters: key, value
+    """
     with open("configuration.json", "r") as read_file:
         data = json.load(read_file)
     with open("configuration.json", "w") as write_file:
@@ -8,11 +12,21 @@ def configuration_save(key, value):
         json.dump(data, write_file)
     
 def configuration_read(key):
+    """ 
+    Read value for key from configuration.json file
+    Parameters: key
+    Return: key's value
+    """
     with open("configuration.json", "r") as read_file:
         data = json.load(read_file)
         return data[key]
 
 def keys_read(key):
+    """ 
+    Read value for key from keys.json file
+    Parameters: key
+    Return: key's value
+    """
     with open("keys.json", "r") as read_file:
         data = json.load(read_file)
         return data[key]
