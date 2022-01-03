db0cb60843f4b559d66817c2100eb3f3e29f13b6
KAROL DUSZCZYK <karol.duszczyk.stud@pw.edu.pl>
2022-01-03 18:51:23 +0100

    keys

--- sensor_node/json_handler.py
+++ sensor_node/json_handler.py
@@ -20,3 +20,24 @@ def configuration_read(key):
     with open("configuration.json", "r") as read_file:
         data = json.load(read_file)
         return data[key]
+
+def keys_read(key):
+    """ 
+    Read value for key from keys.json file
+    Parameters: key
+    Return: key's value
+    """
+    with open("keys.json", "r") as read_file:
+        data = json.load(read_file)
+        return data[key]
+
+def keys_save(key, value):
+    """ 
+    Save key:value pair to keys.json file
+    Parameters: key, value
+    """
+    with open("keys.json", "r") as read_file:
+        data = json.load(read_file)
+    with open("keys.json", "w") as write_file:
+        data[key] = value
+        json.dump(data, write_file)
